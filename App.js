00f9cddd38fc37b796462c37c859da7fd983e198
usman <usmanrehmani126@gmail.com>
2023-10-14 13:15:03 -0700

    Animated progressbar

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { View, StyleSheet, Animated, Text } from "react-native";
+import React, { useEffect, useRef } from "react";
+
+const App = () => {
+  const progressAnimation = useRef(new Animated.Value(0)).current;
+
+  const animateProgress = () => {
+    Animated.timing(progressAnimation, {
+      toValue: 100,
+      duration: 10000,
+      useNativeDriver: false,
+    }).start(({ finished }) => {
+      if (finished) {
+        console.log("Animation trigered");
+      }
+    });
+  };
+
+  useEffect(() => {
+    animateProgress();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: "white", fontSize: 20, marginVertical: 20 }}>
+        React native Custom Progress Bar.{" "}
+      </Text>
+      <View style={styles.progressBg}>
+        <Animated.View
+          style={[
+            styles.progress,
+            {
+              width: progressAnimation.interpolate({
+                inputRange: [0, 100],
+                outputRange: ["0%", "100%"],
+              }),
+            },
+          ]}
+        />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "black",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+  progressBg: {
+    width: "100%",
+    height: 5,
+    backgroundColor: "#708090",
+    borderRadius: 5,
+  },
+  progress: {
+    width: "100%",
+    height: 5,
+    backgroundColor: "white",
+    borderRadius: 5,
   },
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,25 @@
+> ![Demo Video](./assets/video.mp4)
+# React Native
+
+```
+React Native is a popular JavaScript framework that allows you to build native mobile apps using a single codebase. You will learn how to develop cross-platform mobile applications for both iOS and Android platforms, leveraging the efficiency and flexibility of React Native.
+```
+# To create an expo app
+
+```
+expo init or npx create-expo-app appName
+```
+
+# To create React native CLI App
+ 
+```
+npx react-native@latest init appName
+
+```
+# To start server run 
+
+```
+ npm start for expo and npx react-native run-android for React native CLI
+
+```
+# Note:There is no need of any third party Library for this Animation.
